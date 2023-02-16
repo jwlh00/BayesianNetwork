ef483596850c5afb4bebce582690cefd3860a071
jwlh00 <60154594+jwlh00@users.noreply.github.com>
2023-02-15 20:15:01 -0600

    Initial methods

---
+++ Library/BayesianNetwork.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, name, parents=None):
+        self.name = name
+        self.parents = parents or []
+        self.probabilities = {}
+
+    def set_probability(self, probabilities):
+        self.probabilities = probabilities
+
+    def get_probability(self, value, parent_values):
+        key = tuple([value] + parent_values)
+        return self.probabilities.get(key)
+
+class BayesianNetwork:
+    def __init__(self):
+        self.nodes = {}
+
+    def add_node(self, node):
+        self.nodes[node.name] = node
+
+    def set_probability(self, node_name, probabilities):
+        self.nodes[node_name].set_probability(probabilities)
+
+    def get_probability(self, query, evidence):
+        probabilities = {}
+        for value in [True, False]:
+            new_evidence = dict(evidence)
+            new_evidence[query.name] = value
+            probabilities[value] = self.enumerate_all(self.nodes.keys(), new_evidence)
+        return self.normalize(probabilities)
+
+    def enumerate_all(self, variables, evidence):
+        if not variables:
+            return 1
+        first, *rest = list(variables)
+        node = self.nodes[first]
+        if first in evidence:
+            value = evidence[first]
+            parent_values = [evidence[parent] for parent in node.parents]
+            return node.get_probability(value, parent_values) * self.enumerate_all(rest, evidence)
+        else:
+            probabilities = []
+            for value in [True, False]:
+                new_evidence = dict(evidence)
+                new_evidence[first] = value
+                parent_values = [new_evidence[parent] for parent in node.parents]
+                probabilities.append(node.get_probability(value, parent_values) * self.enumerate_all(rest, new_evidence))
+            return sum(probabilities)
+
+    def normalize(self, probabilities):
+        total = sum(probabilities.values())
+        for key in probabilities:
+            probabilities[key] /= total
+        return probabilities
+
+    
+    def compact(self):
+        """
+        Returns a string representing the compact version of the Bayesian network.
+        """
+        node_strings = []
+        for node_name, node in self.nodes.items():
+            if not node.parents:
+                node_string = f"{node_name} ({node.probabilities[(True,)]})"
+            else:
+                parent_strings = [f"{parent_name}={str(parent_value)}" for parent_name, parent_value in zip(node.parents, [True]*len(node.parents))]
+                if (True, True) in node.probabilities:
+                    true_prob = node.probabilities[(True, True)]
+                else:
+                    true_prob = 0.0
+
+                if (True, False) in node.probabilities:
+                    false_prob = node.probabilities[(True, False)]
+                else:
+                    false_prob = 0.0
+
+                node_string = f"{node_name} ({parent_strings[0]}: {true_prob}, not {parent_strings[0]}: {false_prob})"
+                for parent_name, parent_value in zip(node.parents[1:], [True]*len(node.parents[1:])):
+                    node_string = f"{node_string}, {parent_name}={str(parent_value)}: {node.probabilities[(True, True, parent_value)]}, not {parent_name}={str(parent_value)}: {node.probabilities[(True, False, parent_value)]}"
+            node_strings.append(node_string)
+        return "\n".join(node_strings)
---
+++ Library/example.py
@@ -0,0 +1,38 @@
+from BayesianNetwork import BayesianNetwork
+from BayesianNetwork import Node
+
+
+# Create nodes
+burglary = Node("Burglary")
+earthquake = Node("Earthquake")
+alarm = Node("Alarm", parents=["Burglary", "Earthquake"])
+john_calls = Node("JohnCalls", parents=["Alarm"])
+mary_calls = Node("MaryCalls", parents=["Alarm"])
+
+# Create network
+bn = BayesianNetwork()
+bn.add_node(burglary)
+bn.add_node(earthquake)
+bn.add_node(alarm)
+bn.add_node(john_calls)
+bn.add_node(mary_calls)
+
+# Set probabilities
+burglary.set_probability({(True,): 0.001, (False,): 0.999})
+earthquake.set_probability({(True,): 0.002, (False,): 0.998})
+alarm.set_probability({(True, True, True): 0.95, (True, True, False): 0.94, (True, False, True): 0.29, (True, False, False): 0.001, (False, True, True): 0.99, (False, True, False): 0.9, (False, False, True): 0.001, (False, False, False): 0.01})
+john_calls.set_probability({(True, True): 0.9, (True, False): 0.05, (False, True): 0.1, (False, False): 0.95})
+mary_calls.set_probability({(True, True): 0.7, (True, False): 0.01, (False, True): 0.3, (False, False): 0.99})
+alarm.set_probability({(True, True, True): 0.95,(True, True, False): 0.94,(True, False, True): 0.29,(True, False, False): 0.001,(False, True, True): 0.99,(False, True, False): 0.9,(False, False, True): 0.001,(False, False, False): 0.01})
+burglary.set_probability({(True,): 0.001, (False,): 0.999, (True, True): 0, (True, False): 0, (False, True): 0, (False, False): 0})
+
+
+# Get probability of John calling given an alarm
+probability = bn.get_probability(john_calls, {"Alarm": True})
+print("=================================================")
+print("Compact Network:\n", bn.compact())
+print("=================================================")
+print("Bayesian Network factors", probability)
+print("=================================================")
+result = bn.get_probability(john_calls, {"Alarm": True})
+print("Algorithm Result: ", result.get(True))
